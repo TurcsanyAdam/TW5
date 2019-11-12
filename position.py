ae1bc0c67d65624a01312a33f59726e0a49698b5
Erik <szobonyaerik>
2019-11-12 13:49:58 +0100

    fasz kivan

--- position.py
+++ position.py
@@ -2,11 +2,12 @@ import ui
 import data_manager
 import main
 import os
+import company
 
 
 def start_module():
     os.system('cls' if os.name == 'nt' else 'clear')
-    file_name = "application.csv"
+    file_name = "position.csv"
     table = data_manager.import_file(file_name)
 
     menu = ["Create position",
@@ -20,22 +21,42 @@ def start_module():
     x = int(ui.get_input("Enter a number: "))
 
     if x == 1:
-        create_position()
+
+        create_position(file_name)
     elif x == 2:
-        read_position()
+        read_position(table, file_name)
     elif x== 3:
-        read_positions()
+        read_positions(table, file_name)
     elif x == 4:
-        update_position()
+        update_position(table, file_name)
     elif x == 5:
-        delete_position()
+        delete_position(table, file_name)
     elif x == 6:
         main.main()
 
 
 
-def create_position():
-    pass
+def create_position(file_name):
+    '''
+
+    Users can create new positions. A position has an ID, description, number of seats and a “Company ID”.
+    Position IDs are unique amongst other positions.
+    Descriptions cannot be empty.
+    The number of seats must be greater than 0.
+    Company ID must exist.'''
+
+    position_id = ui.generate_random()
+    description = ui.get_input("Write position description: ")
+    number_of_seat = ui.get_input("Enter the number of seats: ")
+    company_id_position = ui.get_input("Enter company id: ")
+    if company_id_position in data_manager.import_file("company.csv"):
+        final_id = [ position_id, description, number_of_seat, company_id_position]
+        data_manager.export_file(final_id, file_name, "a")
+
+
+
+
+
 
 def read_position():
     pass
