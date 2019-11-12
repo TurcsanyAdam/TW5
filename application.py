b69a9d09c82a21474aaee1c1a9251aebb4bc3416
Turcsany Adam <turcsanyadam@gmail.com>
2019-11-12 13:50:43 +0100

    Application

--- application.py
+++ application.py
@@ -11,7 +11,7 @@ def start_module():
 
     menu = ["Create application",
             "Update application",
-            "Delelte application",
+            "Delete application",
             "Back to main menu"
             ]
 
@@ -19,18 +19,20 @@ def start_module():
     x = int(ui.get_input("Enter a number: "))
 
     if x == 1:
-        create_application(table, file_name)
+        create_application(file_name)
     elif x == 2:
-        update_application(table, file_name)
+        application_ID = input("Enter an application ID: ")
+        update_application(table, file_name, application_ID)
     elif x == 3:
-        delete_appliication(table, file_name)
+        application_ID = input("Enter an application ID: ")
+        delete_application(table, file_name, application_ID)
     elif x == 4:
         main.main()
 
 
 
 
-def create_application(table, file_name):
+def create_application(file_name):
     '''
 
     Users can create new applications. An application has an ID, an “accepted” field, a “Student ID” and a “Position ID”.
@@ -38,12 +40,31 @@ def create_application(table, file_name):
     Student and Position IDs must exist.
     The “accepted” field stores whether the application was accepted by a company or not.'''
 
+    application_ID = ui.generate_random()
+    student_ID = input("Enter student ID here: ")
+    position_ID = input("Enter position ID here: ")
+    accepted = "Pending"
+
+    if student_ID in "student.csv":
+        if position_ID in "position.csv":
+            application_data = [application_ID, accepted, student_ID, position_ID]
+            data_manager.export_file(application_data, file_name, "a")
 
 
-    pass
+def update_application(table, file_name, application_ID):
 
-def update_application(table, file_name):
-    pass
+    updated_status = ui.get_input("Enter updated status here: ")
 
-def delete_appliication(table, file_name):
-    pass
+    for i in table:
+        if i[0] == application_ID:
+            i[1] = updated_status
+
+    data_manager.export_file(table, file_name, "w")
+
+
+def delete_application(table, file_name, application_ID):
+    for i in table:
+        if i[0] == application_ID:
+            table.remove(i)
+
+    data_manager.export_file(table, file_name, "w")
